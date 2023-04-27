7a7ccf61c7ec9ad7ce1a99fdccc4c192360d898b
tianhongwei <863062879@qq.com>
2023-04-27 15:51:57 +0800

    test1

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="test cd!" />
+      <HelloWorld msg="test cd githubpages!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
